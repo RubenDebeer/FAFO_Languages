02ff243f81bc4be4ff9973cf6fefd302d4089f00
Ruby Tubes De Beer <66409964+RubenDebeer@users.noreply.github.com>
2025-06-04 11:29:38 +0200

    Added Detail to Binary Search

---
+++ JavaScript/Algorithms/Searching/BinarySearch/Intro/BinarySearch.js
@@ -0,0 +1,32 @@
+// When: 
+// - Use BS whenever you make a BInary decision  to shrink the search range.
+// - Use when yu want to short-circuit and return as soon as it finds the target.
+
+// How: 
+// - With BS you reduce the number of search terms on each iteration.
+
+// Implementation: 
+
+// [1][2][3][4][5][6][7][8][9][10][11][12][13][14][15]
+//  L                       M                       R
+const binary_search = (numbers,target) => {
+
+    let left = 0;
+    let right = numbers.length - 1;
+
+    // <= because left and right could point to the same element, < would miss it
+    while (left <= right) {
+        // use `(right - left) / 2` to prevent `left + right` potential overflow
+        let middle = left + Math.floor((right - left) / 2);
+
+        // Return index if found
+        if(numbers[middle] === target) return middle;
+
+        if(numbers[middle] > target){
+            right = middle - 1; // Discard the Right hand side of the array 
+        }else{
+            left = middle + 1 // Discard the Left hand Side of the Array 
+        }
+    }
+    return -1;
+}
---
+++ JavaScript/Algorithms/Searching/BinarySearch/Intro/FindBoundary.js
@@ -0,0 +1,38 @@
+// This problem is a major 🔑 in solving future binary search-related problems. As we will see in the following modules, many problems boil down to finding the boundary in a boolean array.
+
+
+// When:
+//  - Use this when you’re looking for a threshold or transition point.
+
+// Common UseCases:
+//	•	Find the first version with a bug (e.g., Leetcode’s “First Bad Version”)
+//	•	Find the point where a condition starts to be true
+//	•	Determine the earliest point something becomes valid/active
+
+// How: 
+// - Still works like vanilla BS but just does not short circuit the While loop.
+
+
+//[false, false, false, true, true, true]
+//   L             M                  R
+const findBoundary = (values) => {
+
+    let left = 0;
+    let right = values.length - 1;
+    let boundary_index = -1;
+
+    while (left <= right) {
+
+        let mid = Math.floor((right - left) / 2);
+        // If the values at this index is true then we know we have a candidate, we just don't know if it is the earliest  candidate therefore we can reduce the search to the Right and the right.
+
+        if (values[mid] === true) {
+            boundary_index = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    // If no earliest boundary was found we return the default value.
+    return boundary_index;
+}
